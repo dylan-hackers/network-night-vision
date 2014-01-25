a950150ea01c1944276be56a2f77fe12bbd0a846
Hannes Mehnert <hannes@mehnert.org>
2014-01-25 23:33:11 +0000

    remove unused crap

--- web-sniffer/static/gui.js
+++ web-sniffer/static/gui.js
@@ -260,35 +260,3 @@ function clear_element (element) {
     while (element.hasChildNodes())
         element.removeChild(element.firstChild)
 }
-
-
-
-
-
-function create_context_menu (ele) {
-    ele.onmouseover = function (event) {
-        document.getElementById("contextmenu").className = "show"
-        document.getElementById("contextmenu").style.top =  mouseY(event)
-        document.getElementById("contextmenu").style.left = mouseX(event)
-    }
-    ele.onmouseout = function (event) {
-        document.getElementById("contextmenu").className = "hide"
-    }
-}
-function mouseX (evt) {
-    if (evt.pageX) return evt.pageX;
-    else if (evt.clientX)
-        return evt.clientX + (document.documentElement.scrollLeft ?
-                              document.documentElement.scrollLeft :
-                              document.body.scrollLeft);
-    else return null;
-}
-
-function mouseY (evt) {
-    if (evt.pageY) return evt.pageY;
-    else if (evt.clientY)
-        return evt.clientY + (document.documentElement.scrollTop ?
-                              document.documentElement.scrollTop :
-                              document.body.scrollTop);
-    else return null;
-}
